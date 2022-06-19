4840a21542fe37469e63fc668a8151125f071376
LatanM <106100145+LatanM@users.noreply.github.com>
2022-06-19 21:06:03 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,27 @@
+package Homework3MihaelaLatan;
+
+import java.util.Arrays;
+
+public class Book {
+    String name;
+    int noofpages;
+
+    public Book(String name, int noofpages){
+        this.name = name;
+        this.noofpages = noofpages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNoofpages() {
+        return noofpages;
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + noofpages + " " + "pages" + ")";
+    }
+
+}
